7955572cc1387be7bb860790ca5303022867f1e6
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-11 11:29:22 -0600

    Update grid IDs

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+//----------------------------------------querySelectors-------------------------------
 
+var gameBoard = document.querySelector('.game-board')
+
+
+//----------------------------------------eventListeners-------------------------------
+
+gameBoard.addEventListener('click', function(event){
+    makePlay(event)
+
+})
+
+//----------------------------------------global variables-------------------------------
+
+//----------------------------------------functions-------------------------------
+
+function makePlay(event) {
+    console.log(event.target.id)
+
+}
