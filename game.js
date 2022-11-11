6174f55d0ef47dba9c7c881d85330ddbd5e44b88
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-11 11:00:43 -0600

    Create determineTurn method

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
+class Game {
+    constructor(player1, player2){
+        this.player1 = player1
+        this.player2 = player2
+        this.plays = 0
+        this.turn = 1
+        this.winner = null
+    }
 
+    determineTurn() {
+        this.plays ++
+        if (this.turn === 1){
+            this.turn = player1.id
+        }
+        else {
+            this.turn = player2.id
+        }
+    }
+
+    
+}
